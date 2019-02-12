0423465cae2a60f210d6b5d34fcce083267f2240
Julia Damerow <julia.damerow@googlemail.com>
2019-02-11 17:37:05 -0700

    [WICS-2] remove hibernate.cfg.xml

--- wic/src/main/java/edu/asu/diging/wic/config/PersistentContext.java
+++ wic/src/main/java/edu/asu/diging/wic/config/PersistentContext.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
