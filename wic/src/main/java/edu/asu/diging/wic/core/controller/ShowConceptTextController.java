9b67ef7df218539776e15a13012f7a74085957cd
smit <shahsmit49@gmail.com>
2021-02-23 22:09:32 -0700

    [WICS-6] javadoc moved to interface & some optimization

--- wic/src/main/java/edu/asu/diging/wic/core/controller/ShowConceptTextController.java
+++ wic/src/main/java/edu/asu/diging/wic/core/controller/ShowConceptTextController.java
@@ -24,8 +24,9 @@ public class ShowConceptTextController {
 		
 		List<ConceptText> allConceptText = conceptTextService.findAll(pageNumber);
 		model.addAttribute("allConceptText",allConceptText);
-		int pagesCount = conceptTextService.getCount() % 5 == 0 ? 
-		        (conceptTextService.getCount()/5) : (conceptTextService.getCount()/5) + 1;
+		int totalEntriesInDb = conceptTextService.getTextCount();
+		int pagesCount = totalEntriesInDb % 5 == 0 ? 
+		        (totalEntriesInDb/5) : (totalEntriesInDb/5) + 1;
 		model.addAttribute("totalPages", pagesCount);
 		model.addAttribute("currentPageNumber", Integer.parseInt(pageNumber));
 		return "admin/text/list";
--- wic/src/main/java/edu/asu/diging/wic/core/service/IConceptTextService.java
+++ wic/src/main/java/edu/asu/diging/wic/core/service/IConceptTextService.java
@@ -6,15 +6,48 @@ import edu.asu.diging.wic.core.model.impl.ConceptText;
 
 public interface IConceptTextService {
 	
+    /**
+     * <p>Method is used to add conceptText model/object in database
+     * </p>
+     * @param conceptText Model of conceptText which will be stored 
+     */
 	void addText(ConceptText conceptText);
 
+	/**
+     * <p>Method is used to find all conceptText to be shown on a particular page
+     * </p>
+     * @param Pagenumber the pageNumber which is to be displayed
+     * @return conceptText List of conceptText belonging to that pageNumber
+     */
 	List<ConceptText> findAll(String pageNumber);
 	
+	/**
+     * <p>Method used to delete a particular conceptText
+     * </p>
+     * @param id id of the conceptText model which is to be deleted
+     */
 	void deleteText(String id);
 	
+	/**
+     * <p>Method used to update a particular conceptText
+     * </p>
+     * @param updatedForm conceptText Model with updated information
+     * @param modifiedBy the user who modified the conceptText
+     */
 	void updateText(ConceptText updatedForm, String modifiedBy);
 	
+	/**
+     * <p>Method used to update a particular single conceptText by id 
+     * </p>
+     * @param id id of the conceptText whose details you want to fetch
+     * @return conceptText model of the conceptText fetched from id
+     */
 	ConceptText getTextById(String id);
 	
-	int getCount();
+	/**
+     * <p>Method used to get total count from database
+     * </p>
+     * @return count of the number of conceptText in database
+     */
+	int getTextCount();
 }
