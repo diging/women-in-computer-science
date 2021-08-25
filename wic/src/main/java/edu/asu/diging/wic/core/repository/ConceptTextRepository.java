148b41a1eecc82f47f3c5d4353af280a559339de
Maulik Vajubhai Limbadiya (Student) <mlimbadi@sundevils.asu.edu>
2021-08-25 09:37:14 -0700

    [WICS-6] - removed javers annotation from concept text repository

--- wic/src/main/java/edu/asu/diging/wic/core/repository/ConceptTextRepository.java
+++ wic/src/main/java/edu/asu/diging/wic/core/repository/ConceptTextRepository.java
@@ -1,13 +1,11 @@
 package edu.asu.diging.wic.core.repository;
 
-import org.javers.spring.annotation.JaversSpringDataAuditable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import edu.asu.diging.wic.core.model.impl.ConceptText;
 
 @Repository
-@JaversSpringDataAuditable
 public interface ConceptTextRepository extends PagingAndSortingRepository<ConceptText, Long> {
 
 }
